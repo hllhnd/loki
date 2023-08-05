4e47d81cccb7a460d4e1e84fac8bb3c6294cbb0e
Reperak <reperak5fba@protonmail.com>
2023-08-05 17:22:48 -0500

    Implement basic CLI
    Closes #1

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod executable;
 mod node;
 
 use std::cell::RefCell;
+use std::env::args;
 use std::env::current_dir;
 use std::error::Error;
 use std::fs::File;
@@ -26,8 +27,51 @@ use crate::config::Project;
 fn main() -> Result<(), Report> {
     color_eyre::install()?;
 
-    print!("{}", toml::to_string_pretty(&Project::default())?);
+    let args = args().collect::<Vec<_>>();
+    match args.get(1).map(|f| f.as_str()) {
+        Some("build") => {
+            build_project()?;
+        },
 
+        Some("-v" | "--version") => {
+            #[rustfmt::skip]
+            println!(
+                "The Loki Build System, version 0.1.0\n\
+                \n\
+                Copyright (c) 2023 Reperak\n\
+                \n\
+                Loki is free software licensed under the GNU GPL version 3 or later.\n\
+                \n\
+                If you did not receive a copy of the license with this program, you may obtain\n\
+                one at <http://gnu.org/licenses/gpl.html>."
+            );
+        },
+
+        Some("-h" | "--help") | None => {
+            #[rustfmt::skip]
+            println!(
+                "The Loki Build System\n\
+                \n\
+                Copyright (c) 2023 Reperak\n\
+                \n\
+                Subcommands:\n    \
+                    build           Build a Loki project\n\
+                \n\
+                Usage:\n    \
+                    --help          Show this text and exit\n    \
+                    --version       Show version information"
+            );
+        },
+
+        _ => {
+            println!("Unknown command/flag '{}'. See '--help' for usage.", args[1]);
+        },
+    }
+
+    Ok(())
+}
+
+fn build_project() -> Result<(), Report> {
     let (loki_toml, source_directory, target_directory, object_directory) = current_dir()?
         .ancestors()
         .map(PathBuf::from)
