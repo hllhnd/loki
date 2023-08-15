8ec383355b80fac06aac8f4b7b877befa52f77b1
Reperak <reperak5fba@protonmail.com>
2023-08-15 15:47:06 -0500

    Begin rewrite

--- src/node.rs
+++
@@ -1,9 +0,0 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-
-use crate::executable::Executable;
-
-pub struct Node {
-    pub executable: Box<dyn Executable>,
-    pub children:   Vec<Rc<RefCell<Node>>>,
-}
