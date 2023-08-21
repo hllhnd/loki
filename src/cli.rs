b5d76ffb2578fac7a7b8b88ad59777b89f66bc71
Reperak <reperak5fba@protonmail.com>
2023-08-21 13:45:44 -0500

    Split CLI dispatching into individual functions

--- src/cli.rs
+++ src/cli.rs
@@ -6,57 +6,64 @@ use indoc::indoc;
 use crate::build_project;
 use crate::config::Project;
 
+/// Process the command line arguments and run the appropriate subcommand, assuming command arguments start at index 1.
 pub fn process(args: &[String]) -> Result<(), Report> {
     match args.get(1).map(|f| f.as_str()) {
-        Some("build") => {
-            build_project()?;
-        },
+        Some("build") => subcommand_build(),
+        Some("new") => subcommand_new(args.get(2)),
+        Some("-v" | "--version") => subcommand_version(),
+        Some("-h" | "--help") | None => subcommand_help(),
+        _ => Ok(println!("Unknown command/flag '{}'. See '--help' for usage.", args[1])),
+    }
+}
 
-        Some("-v" | "--version") => {
+fn subcommand_build() -> Result<(), Report> {
+    build_project()
+}
+
+fn subcommand_new(name: Option<&String>) -> Result<(), Report> {
+    match name {
+        Some(name) => Project::with_name(name).generate_at(current_dir()?),
+        None => {
             print!(indoc! {"
-                The Loki Build System, version 0.0.1
+                error: missing argument 'name'
 
-                Copyright (c) 2023 Reperak
-
-                Loki is free software licensed under the GNU GPL version 3 or later.
-
-                If you did not receive a copy of the license with this program, you may obtain
-                one at <http://gnu.org/licenses/gpl.html>.
+                Usage: loki new <name>
             "});
+            Ok(())
         },
+    }
+}
 
-        Some("-h" | "--help") | None => {
-            print!(indoc! {"
-                The Loki Build System
+fn subcommand_version() -> Result<(), Report> {
+    print!(indoc! {"
+        The Loki Build System, version 0.0.1
 
-                Copyright (c) 2023 Reperak
+        Copyright (c) 2023 Reperak
 
-                Subcommands:
-                    build           Build a Loki project
-                    new             Create a new Loki project
+        Loki is free software licensed under the GNU GPL version 3 or later.
 
-                Usage:
-                    --help          Show this text and exit
-                    --version       Show version information
-            "});
-        },
+        If you did not receive a copy of the license with this program, you may obtain
+        one at <http://gnu.org/licenses/gpl.html>.
+    "});
 
-        Some("new") =>
-            if let Some(name) = args.get(2) {
-                // TODO: allow for creating projects in other directories
-                Project::with_name(name).generate_at(current_dir()?)?;
-            } else {
-                print!(indoc! {"
-                    error: missing argument 'name'
+    Ok(())
+}
 
-                    Usage: loki new <name>
-                "});
-            },
+fn subcommand_help() -> Result<(), Report> {
+    print!(indoc! {"
+        The Loki Build System
 
-        _ => {
-            println!("Unknown command/flag '{}'. See '--help' for usage.", args[1]);
-        },
-    };
+        Copyright (c) 2023 Reperak
+
+        Subcommands:
+            build           Build a Loki project
+            new             Create a new Loki project
+
+        Usage:
+            --help          Show this text and exit
+            --version       Show version information
+    "});
 
     Ok(())
 }
