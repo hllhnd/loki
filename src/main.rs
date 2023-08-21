b84ddcf93bc78c1002d2cf323b639253f4ac77ba
Reperak <reperak5fba@protonmail.com>
2023-08-21 13:03:46 -0500

    Extract CLI code from main into a module
    Closes #12

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod cli;
 mod compiler;
 mod config;
 mod directory;
@@ -28,59 +29,8 @@ fn main() -> Result<(), Report> {
     color_eyre::install()?;
 
     let args = args().collect::<Vec<_>>();
-    match args.get(1).map(|f| f.as_str()) {
-        Some("build") => {
-            build_project()?;
-        },
 
-        Some("-v" | "--version") => {
-            #[rustfmt::skip]
-            println!(
-                "The Loki Build System, version 0.1.0\n\
-                \n\
-                Copyright (c) 2023 Reperak\n\
-                \n\
-                Loki is free software licensed under the GNU GPL version 3 or later.\n\
-                \n\
-                If you did not receive a copy of the license with this program, you may obtain\n\
-                one at <http://gnu.org/licenses/gpl.html>."
-            );
-        },
-
-        Some("-h" | "--help") | None => {
-            #[rustfmt::skip]
-            println!(
-                "The Loki Build System\n\
-                \n\
-                Copyright (c) 2023 Reperak\n\
-                \n\
-                Subcommands:\n    \
-                    build           Build a Loki project\n    \
-                    new             Create a new Loki project\n\
-                \n\
-                Usage:\n    \
-                    --help          Show this text and exit\n    \
-                    --version       Show version information"
-            );
-        },
-
-        Some("new") =>
-            if let Some(name) = args.get(2) {
-                // TODO: allow for creating projects in other directories
-                Project::with_name(name).generate_at(current_dir()?)?;
-            } else {
-                #[rustfmt::skip]
-                println!(
-                    "error: missing argument 'path'\n\
-                    \n\
-                    Usage: loki new <path>"
-                );
-            },
-
-        _ => {
-            println!("Unknown command/flag '{}'. See '--help' for usage.", args[1]);
-        },
-    }
+    cli::process(&args)?;
 
     Ok(())
 }
